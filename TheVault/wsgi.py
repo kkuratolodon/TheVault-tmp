9023be3f7bea813ba7037154f4ec5314543ab617
kkuratolodon <mif05123@gmail.com>
2023-09-09 22:36:34 +0700

    undo last commit

--- TheVault/wsgi.py
+++ TheVault/wsgi.py
@@ -9,13 +9,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
-from django.contrib.auth.models import User
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TheVault.settings')
 
 application = get_wsgi_application()
-
-users = User.objects.all()
-if not users:
-    User.objects.create_superuser(username="kkura", email="user@example.com", password="rafliwibu", is_active=True, is_staff=True)
