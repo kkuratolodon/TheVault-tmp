80059c611e8760889e7d6704cf39ca535a8958b7
kkuratolodon <mif05123@gmail.com>
2023-09-09 22:49:03 +0700

    editing something

--- main/admin.py
+++ main/admin.py
@@ -4,3 +4,15 @@ from .models import Album
 
 # Register your models here.
 admin.site.register(Album)
+
+from django.contrib.auth.models import User
+
+username = 'admin';
+password = 'rafliwibu';
+email = 'tes@gmail.com';
+
+if User.objects.filter(username=username).count()==0:
+    User.objects.create_superuser(username, email, password);
+    print('Superuser created.');
+else:
+    print('Superuser creation skipped.')
